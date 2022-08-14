7f6481a00e309aeaa2167fe28ddc81109c826049
nhandt <nhandt@nexlesoft.com>
2022-08-14 12:06:09 +0700

    Add bookmark APIs

--- src/bookmark/bookmark.controller.ts
+++ src/bookmark/bookmark.controller.ts
@@ -1,4 +1,58 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { JwtGuard } from '../auth/guard';
+import { BookmarkService } from './bookmark.service';
+import { GetUser } from '../auth/decorator';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
+@UseGuards(JwtGuard)
 @Controller('bookmark')
-export class BookmarkController {}
+export class BookmarkController {
+  constructor(private bookmarkService: BookmarkService) {}
+  @Get()
+  getBookmarks(@GetUser('id', ParseIntPipe) userId: number) {
+    return this.bookmarkService.getBookmarks(userId);
+  }
+
+  @Get(':id')
+  getBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id') bookmarkId: number,
+  ) {
+    return this.bookmarkService.getBookmarkById(userId, bookmarkId);
+  }
+
+  @Post()
+  createBookmark(
+    @GetUser('id') userId: number,
+    @Body() dto: CreateBookmarkDto,
+  ) {
+    return this.bookmarkService.createBookmark(userId, dto);
+  }
+
+  @Patch(':id')
+  editBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id') bookmarkId: number,
+    @Body() dto: EditBookmarkDto,
+  ) {
+    return this.bookmarkService.editBookmarkById(userId, bookmarkId, dto);
+  }
+
+  @Delete(':id')
+  deleteBookmarkById(
+    @GetUser('id') userId: number,
+    @Param('id') bookmarkId: number,
+  ) {
+    return this.bookmarkService.deleteBookmarkById(userId, bookmarkId);
+  }
+}
--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { CreateBookmarkDto, EditBookmarkDto } from './dto';
 
 @Injectable()
-export class BookmarkService {}
+export class BookmarkService {
+  getBookmarks(userId: number) {}
+
+  getBookmarkById(userId: number, bookmarkId: number) {}
+
+  createBookmark(userId: number, dto: CreateBookmarkDto) {}
+
+  editBookmarkById(userId: number, bookmarkId: number, dto: EditBookmarkDto) {}
+
+  deleteBookmarkById(userId: number, bookmarkId: number) {}
+}
