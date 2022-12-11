9642a1d84b7de32e8a841292f87fccc3a8783280
nhandt <nhandt@nexlesoft.com>
2022-12-11 22:51:43 +0700

    Test update

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -55,7 +55,7 @@ describe('App (e2e)', () => {
           .expectStatus(400);
       });
 
-      it('should throw if password is empty', () => {
+      it('Should throw if password is empty', () => {
         return pactum
           .spec()
           .post('/auth/signup')
